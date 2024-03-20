8bf5907077ddacf9b06fae049aae063c448adf7e
Mark Purdon <mark@hewmann.com>
2024-03-20 12:59:56 +0000

    removed bin

--- obj/Debug/net9.0/TextReplace.AssemblyInfo.cs
+++ obj/Debug/net9.0/TextReplace.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextReplace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b058c57db5afd130db2e28af87721e55e313864")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextReplace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextReplace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
